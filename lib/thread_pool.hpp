16455e3c9b2947c02d350b2b8d53e788fe89caf1
Joel Bodenmann <jbo@insane.engineer>
2024-05-26 03:49:10 +0200

    Refactor: Place queue into separate source file

--- lib/thread_pool.hpp
+++ lib/thread_pool.hpp
@@ -1,87 +1,20 @@
 #pragma once
 
+#include "queue.hpp"
+
 #include <atomic>
+#include <concepts>
 #include <condition_variable>
 #include <functional>
 #include <future>
 #include <memory>
 #include <mutex>
-#include <queue>
-#include <shared_mutex>
 #include <thread>
 #include <vector>
 #include <type_traits>
 
 namespace jbo
 {
-
-    /**
-     * A blocking queue implementation.
-     *
-     * @details This uses an std::shared_mutex to facilitate read vs. write blocking in a simple manner.
-     *          Certain actions, such as checking the size can be done by multiple threads simultaneously as long as no
-     *          write operations are happening. Only one write operation may occur at any given time.
-     */
-    template<
-        std::movable T,
-        typename Queue = std::queue<T>
-    >
-    class queue
-    {
-    public:
-        [[nodiscard]]
-        bool
-        empty() const
-        {
-            read_lock lock(m_mtx);
-            return m_queue.empty();
-        }
-
-        [[nodiscard]]
-        std::size_t
-        size() const
-        {
-            read_lock lock(m_mtx);
-            return std::size(m_queue);
-        }
-
-        void
-        clear()
-        {
-            write_lock lock(m_mtx);
-            m_queue.clear();
-        }
-
-        template<typename ...Args>
-        void
-        emplace(Args&& ...args)
-        {
-            write_lock lock(m_mtx);
-            m_queue.emplace(std::forward<Args>(args)...);
-        }
-
-        [[nodiscard]]
-        bool
-        try_pop(T& t)
-        {
-            write_lock lock(m_mtx);
-
-            if (m_queue.empty())
-                return false;
-
-            t = std::move(m_queue.front());
-            m_queue.pop();
-
-            return true;
-        }
-
-    private:
-        using read_lock = std::scoped_lock<std::shared_mutex>;
-        using write_lock = std::shared_lock<std::shared_mutex>;
-
-        Queue m_queue;
-        mutable std::shared_mutex m_mtx;
-    };
 
     /**
      * A simple to use, robust and flexible C++ thread pool.
